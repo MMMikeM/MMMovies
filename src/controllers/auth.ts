30869fe10f9e27044bc14f26e68af937921ce467
MMMike <mikem.tba@gmail.com>
2021-05-04 22:17:30 +0200

    feat: :lock: Changed to session auth

--- src/controllers/auth.ts
+++ src/controllers/auth.ts
@@ -1,15 +1,22 @@
-import { PrismaClient } from "@prisma/client";
-import jwt from "jsonwebtoken";
-import argon2 from "argon2";
+import { PrismaClient } from "@prisma/client"
+import argon2 from "argon2"
+const authServices = require("../services/auth")
 
-const prisma = new PrismaClient();
+const prisma = new PrismaClient()
+
+export const getLogin = (req, res) => {
+  res.render("login")
+}
+
+export const getSignUp = (req, res) => {
+  res.render("signup")
+}
 
 const createUser = async (firstName, lastName, email, password) => {
-  console.log("yo");
   const data = {
     email,
     hashedPassword: await argon2.hash(password),
-  };
+  }
   const result = await prisma.user.create({
     data: {
       firstName: firstName,
@@ -17,64 +24,44 @@ const createUser = async (firstName, lastName, email, password) => {
       email: data.email,
       password: data.hashedPassword,
     },
-  });
-  return result;
-};
-
-export const getLogin = (req, res) => {
-  res.render("login");
-};
-
-export const postLogin = async (req, res) => {
-  try {
-    const { email, password } = req.body;
-    const user = await prisma.user.findUnique({
-      where: { email: email },
-    });
-
-    if (user) {
-      const validPassword = argon2.verify(user.password, password);
-
-      const token = jwt.sign({ email }, "supersecure", { expiresIn: 1000000 });
-      const userToReturn: any = { ...user, ...{ token } };
-
-      delete userToReturn.password;
-      console.log("I think this is a valid sign in?");
-      res.status(200).json(userToReturn);
-    }
-  } catch (error) {
-    console.error(error);
-  }
-  // res.render("signup");
-};
-
-export const getSignUp = (req, res) => {
-  res.render("signup");
-};
+  })
+  return result
+}
 
 export const postSignup = async (req, res) => {
   try {
-    const { firstName, lastName, email, password } = req.body;
+    const { firstName, lastName, email, password } = req.body
     const user = await prisma.user.findUnique({
       where: { email: email },
-    });
+    })
 
     if (!user) {
-      await createUser(firstName, lastName, email, password);
-      const newUser: any = await prisma.user.findUnique({
-        where: {
-          email: email,
-        },
-      });
-
-      const token = jwt.sign({ email }, "supersecure", { expiresIn: 1000000 });
-      const userToReturn = { ...newUser, ...{ token } };
-
-      delete userToReturn.password;
-      res.status(200).json(userToReturn);
+      const newUser = await createUser(firstName, lastName, email, password)
     }
   } catch (error) {
-    console.error(error);
+    console.error(error)
   }
-  // res.render("signup");
-};
+  res.render("signup")
+}
+
+export const checkAuth = (req, res, next) => {
+  if (req.isAuthenticated()) {
+    return next()
+  }
+  res.redirect("/login")
+}
+
+export const checkNotAuth = (req, res, next) => {
+  if (req.isAuthenticated()) {
+    return res.redirect("/")
+  }
+  next()
+}
+
+export const getLanding = (req, res, next) => {
+  if (req.isAuthenticated()) {
+    res.render("layout")
+  } else {
+    res.render("index")
+  }
+}
--- src/services/auth.ts
+++ src/services/auth.ts
@@ -1,22 +1,31 @@
-import { PrismaClient } from "@prisma/client";
-import { Strategy, ExtractJwt } from "passport-jwt";
+import { PrismaClient } from "@prisma/client"
+import argon2 from "argon2"
+import { Strategy } from "passport-local"
 
-const prisma = new PrismaClient();
+const prisma = new PrismaClient()
 
-export const applyPassportStrategy = (passport) => {
-  const options = { jwtFromRequest: "", secretOrKey: "" };
-  options.jwtFromRequest = ExtractJwt.fromAuthHeaderAsBearerToken();
-  options.secretOrKey = "SuperSecure";
-  passport.use(
-    new Strategy(options, async (payload, done) => {
-      const user: any = await prisma.user.findUnique({
-        where: { email: payload.email },
-      });
-
-      return done(null, {
-        email: user.email,
-        id: user.id,
-      });
-    })
-  );
-};
+export const authenticateUser = async (email, password, done) => {
+  const user: any = await prisma.user.findUnique({
+    where: { email: email },
+  })
+  if (user) {
+    try {
+      if (await argon2.verify(user.password, password)) {
+        console.log("verified")
+        return done(null, {
+          email: user.email,
+          id: user.id,
+        })
+      } else {
+        console.log("wrong password error")
+        return done(null, false, { message: "Incorrect login details" })
+      }
+    } catch (error) {
+      console.log("Couldn't try!")
+      return done(error)
+    }
+  } else {
+    console.log("No user exists error")
+    return done(null, false, { message: "Incorrect login details" })
+  }
+}
