401ccff07f00af1bc026152e2a6a27a3794fb098
MMMike <mikem.tba@gmail.com>
2021-05-04 23:27:10 +0200

    feat: :lock: Referenced .env and added an example

--- src/loaders/authLoader.ts
+++ src/loaders/authLoader.ts
@@ -1,16 +1,19 @@
-const passport = require("passport")
-const session = require("express-session")
-const Strategy = require("passport-local").Strategy
-const authServices = require("../services/auth")
+// const passport = require("passport")
+import passport from "passport"
+import session from "express-session"
+import Strategy from "passport-local"
+import * as authServices from "../services/auth"
+import dotenv from "dotenv"
 import { PrismaClient } from "@prisma/client"
 
 const prisma = new PrismaClient()
 
 export default async ({ app }) => {
+  const env: any = dotenv.config().parsed
   // Initialise sessions using passport
   app.use(
     session({
-      secret: "SuperSecure",
+      secret: env.SECRET,
       resave: true,
       saveUninitialized: true,
       cookie: { secure: false },
