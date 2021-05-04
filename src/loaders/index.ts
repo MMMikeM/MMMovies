9171048a83b95697744ce4ba5646525106562035
MMMike <mikem.tba@gmail.com>
2021-05-04 22:20:01 +0200

    feat: :sparkles: Added passport initialisation to loader

--- src/loaders/index.ts
+++ src/loaders/index.ts
@@ -1,63 +1,89 @@
-const express = require("express");
-const path = require("path");
-const passport = require("passport");
+const express = require("express")
+const path = require("path")
+const passport = require("passport")
+const session = require("express-session")
+const Strategy = require("passport-local").Strategy
+const flash = require("express-flash")
+const methodOverride = require("method-override")
 
-const routeFile = require("../routes");
-const { applyPassportStrategy } = require("../services/auth");
+const routeFile = require("../routes")
+const authServices = require("../services/auth")
+
+import { PrismaClient } from "@prisma/client"
+const prisma = new PrismaClient()
 
 export default async ({ app }) => {
   // Endpoint to check status
   app.get("/status", (req, res) => {
-    res.status(200).end();
-  });
+    res.status(200).end()
+  })
 
-  // Apply passport strategy
-  applyPassportStrategy(passport);
+  // Initialise sessions using passport
+  app.use(
+    session({
+      secret: "SuperSecure",
+      resave: true,
+      saveUninitialized: true,
+      cookie: { secure: false },
+    })
+  )
+
+  passport.use(
+    new Strategy(
+      { usernameField: "email", passwordField: "password" },
+      authServices.authenticateUser
+    )
+  )
+
+  app.use(passport.initialize())
+  app.use(passport.session())
+
+  passport.serializeUser((user, done) => {
+    done(null, user.id)
+  })
+
+  passport.deserializeUser(async (id, done) => {
+    try {
+      const user = await prisma.user.findUnique({
+        where: {
+          id: id,
+        },
+      })
+      return done(null, user)
+    } catch (error) {
+      return done(error, null)
+    }
+  })
 
   // Set views & view engine
-  app.set("views", path.join(__dirname, "../views"));
-  app.set("view engine", "pug");
+  app.set("views", path.join(__dirname, "../views"))
+  app.set("view engine", "pug")
+  app.use(flash())
 
   // Serve static files - for images & packages
-  app.use(express.static(path.join(__dirname, "../public")));
+  app.use(express.static(path.join(__dirname, "../public")))
 
-  // parse encoded requests
-  app.use(express.urlencoded({ extended: true }));
+  // parse encoded requests & custom methods
+  app.use(express.json())
+  app.use(express.urlencoded({ extended: true }))
+  app.use(methodOverride("_method"))
 
   // Start app
   app.listen(3000, () => {
-    console.log("Server running");
-  });
+    console.log("Server running")
+  })
 
   // Import routes
-  app.use("/", routeFile);
-
-  // Handle not found pages
-  app.use((req, res, next) => {
-    const err = new Error("Not Found");
-    err["status"] = 404;
-    next(err);
-  });
-
-  /// error handlers
-  app.use((err, req, res, next) => {
-    /**
-     * Handle 401 thrown by express-jwt library
-     */
-    if (err.name === "UnauthorizedError") {
-      return res.status(err.status).send({ message: err.message }).end();
-    }
-    return next(err);
-  });
+  app.use("/", routeFile)
 
   app.use((err, req, res, next) => {
-    res.status(err.status || 500);
+    res.status(err.status || 500)
     res.json({
       errors: {
         message: err.message,
       },
-    });
-  });
+    })
+  })
 
-  return app;
-};
+  return app
+}
