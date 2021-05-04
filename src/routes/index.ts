514a027e03996cb89eddcc793605d4ca8003ee15
MMMike <mikem.tba@gmail.com>
2021-05-04 22:18:50 +0200

    feat: :sparkles: Improved routes, including auth checkers

--- src/routes/index.ts
+++ src/routes/index.ts
@@ -1,46 +1,55 @@
-const routes = require("express").Router();
-import { PrismaClient } from "@prisma/client";
-import * as searchController from "../controllers/search";
-import * as authController from "../controllers/auth";
+const routes = require("express").Router()
+import { PrismaClient } from "@prisma/client"
+import * as searchController from "../controllers/search"
+import * as authController from "../controllers/auth"
+const passport = require("passport")
 
-const prisma = new PrismaClient();
+const prisma = new PrismaClient()
 
-routes.get("/", async (req, res) => {
-  const users = await prisma.user.findMany();
-  res.render("index", {
-    title: "MMMovies",
-    message: JSON.stringify(users),
-  });
-});
+routes.get("/", authController.getLanding)
 
-routes.get("/search", searchController.moviePage);
+routes.get("/search", searchController.moviePage)
 
-routes.post("/search", searchController.searchMovie);
+routes.post("/search", searchController.searchMovie)
 
 routes.get("/to_watch", (req, res) => {
   res.render("towatch", {
     title: "MMMovies to watch",
     message: "You have navigated to things!!!",
-  });
-});
+  })
+})
 
 routes.get("/watched", (req, res) => {
   res.render("watched", {
     title: "MMMovies I have watched",
     message: "You have navigated to things!!!",
-  });
-});
+  })
+})
 
-routes.get("/login", authController.getLogin);
+routes.get("/login", authController.getLogin)
 
-routes.post("/login", authController.postLogin);
+routes.post(
+  "/login",
+  authController.checkNotAuth,
+  passport.authenticate("local", {
+    successRedirect: "/",
+    failureRedirect: "/login",
+    failureFlash: true,
+  })
+)
 
-routes.get("/sign_up", authController.getSignUp);
+routes.get("/sign_up", authController.getSignUp)
 
-routes.post("/sign_up", authController.postSignup);
+routes.post("/sign_up", authController.postSignup)
 
 routes.get("/reset", (req, res) => {
-  res.render("reset");
-});
+  res.render("reset")
+})
 
-module.exports = routes;
+routes.delete("/logout", (req, res) => {
+  console.log("signingout!")
+  req.logOut()
+  res.redirect("/login")
+})
+
+module.exports = routes
